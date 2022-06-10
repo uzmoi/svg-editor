75cfe9db6007575224aafd05fe524315acc1bd24
rizzzse <rizzzse@gmail.com>
2022-06-10 21:26:08 +0900

    init halogen

--- spago.dhall
+++ spago.dhall
@@ -1,5 +1,16 @@
 { name = "svg-editor"
-, dependencies = [ "console", "effect", "prelude" ]
+, dependencies =
+  [ "aff"
+  , "console"
+  , "effect"
+  , "exceptions"
+  , "halogen"
+  , "maybe"
+  , "prelude"
+  , "transformers"
+  , "web-dom"
+  , "web-html"
+  ]
 , packages = ./packages.dhall
 , sources = [ "src/**/*.purs", "test/**/*.purs" ]
 }
--- src/Main.purs
+++ src/Main.purs
@@ -1,10 +1,55 @@
-module Main where
+module Main (main) where
 
 import Prelude
-
+import Control.Monad.Error.Class (throwError)
 import Effect (Effect)
-import Effect.Console (log)
+import Effect.Exception (error)
+import Data.Maybe (maybe)
+import Halogen as H
+import Halogen.Aff as HA
+import Halogen.HTML as HH
+import Halogen.HTML.Events as HE
+import Halogen.VDom.Driver (runUI)
+import Effect.Aff (Aff)
+import Web.DOM.ParentNode (QuerySelector(..))
+import Web.HTML.HTMLElement (HTMLElement)
+
+awaitSelect :: QuerySelector -> Aff HTMLElement
+awaitSelect querySelector@(QuerySelector str) = do
+  HA.awaitLoad
+  element <- HA.selectElement $ querySelector
+  maybe (throwError $ error $ "Could not find '" <> str <> "'") pure element
 
 main :: Effect Unit
-main = do
-  log "🍝"
+main =
+  HA.runHalogenAff do
+    appRoot <- awaitSelect $ QuerySelector "#app"
+    runUI component unit appRoot
+
+type Params
+  = Unit
+
+data Action
+  = Increment
+  | Decrement
+
+component :: forall query message. H.Component query Params message Aff
+component =
+  H.mkComponent
+    { initialState
+    , render
+    , eval: H.mkEval $ H.defaultEval { handleAction = handleAction }
+    }
+  where
+  initialState _ = { count: 0 }
+
+  render { count } =
+    HH.div_
+      [ HH.button [ HE.onClick \_ -> Decrement ] [ HH.text "-" ]
+      , HH.div_ [ HH.text $ show count ]
+      , HH.button [ HE.onClick \_ -> Increment ] [ HH.text "+" ]
+      ]
+
+  handleAction = case _ of
+    Increment -> H.modify_ \{ count } -> { count: count + 1 }
+    Decrement -> H.modify_ \{ count } -> { count: count - 1 }

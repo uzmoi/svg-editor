b50b465e39c053565276fc641d5cf109444524b5
rizzzse <rizzzse@gmail.com>
2022-06-13 01:29:53 +0900

    add select feature

--- src/SvgEditor/View/Root.purs
+++ src/SvgEditor/View/Root.purs
@@ -1,11 +1,12 @@
 module SvgEditor.View.Root (appRoot) where
 
 import Prelude
-import Data.Array (filter, snoc)
+import Data.Array (filter, find, snoc)
 import Data.Maybe (Maybe(..))
 import Halogen as H
 import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
+import Halogen.HTML.Properties as HP
 import Halogen.Svg.Elements as HSE
 import Halogen.Svg.Attributes.Path as SP
 import Effect.Aff (Aff)
@@ -19,6 +20,8 @@ type Params
 data Action
   = AddLayer
   | DeleteLayer Int
+  | SelectLayer Int
+  | EditLayerName Int String
 
 appRoot :: forall query message. H.Component query Params message Aff
 appRoot =
@@ -43,27 +46,39 @@ appRoot =
             }
         }
     , layers: []
+    , selectedLayer: -1
     }
 
-  render { canvas, layers } =
+  render { canvas, layers, selectedLayer } =
     HH.div_
       [ HSE.svg (canvasProps canvas) $ map layer layers
       , HH.ul_
           $ map
-              ( \layer ->
+              ( \{ id, name } ->
                   HH.li_
-                    [ HH.div_
-                        [ HH.text layer.name
-                        , HH.button
-                            [ HE.onClick $ \_ -> DeleteLayer layer.id ]
-                            [ HH.text "delete layer" ]
+                    [ HH.div
+                        [ HE.onClick $ \_ -> SelectLayer id
+                        , HP.class_ $ HH.ClassName if id == selectedLayer then "selected" else ""
                         ]
+                        [ HH.text name ]
                     ]
               )
               layers
       , HH.button
           [ HE.onClick $ \_ -> AddLayer ]
           [ HH.text "add layer" ]
+      , case find (\layer -> layer.id == selectedLayer) layers of
+          Just { id, name } ->
+            HH.div_
+              [ HH.input
+                  [ HP.value name
+                  , HE.onValueInput $ EditLayerName id
+                  ]
+              , HH.button
+                  [ HE.onClick $ \_ -> DeleteLayer id ]
+                  [ HH.text "delete layer" ]
+              ]
+          Nothing -> HH.div_ []
       ]
 
   handleAction = case _ of
@@ -86,4 +101,17 @@ appRoot =
       H.modify_ \state ->
         state
           { layers = filter (\layer -> layer.id /= id) state.layers
+          }
+    SelectLayer id ->
+      H.modify_ \state ->
+        state { selectedLayer = if state.selectedLayer == id then -1 else id }
+    EditLayerName id name ->
+      H.modify_ \state ->
+        state
+          { layers =
+            map
+              ( \layer ->
+                  if layer.id == id then layer { name = name } else layer
+              )
+              state.layers
           }

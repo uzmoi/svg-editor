60cfd1f430ef381d56f6cccf6568f32a5c678a1a
rizzzse <rizzzse@gmail.com>
2022-06-19 19:30:14 +0900

    remove @originjs/vite-plugin-commonjs

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,5 @@
 import { defineConfig } from "vite";
-import { viteCommonjs } from "@originjs/vite-plugin-commonjs";
 
 export default defineConfig({
-    plugins: [
-        viteCommonjs(),
-    ],
     base: "/svg-editor/",
 });

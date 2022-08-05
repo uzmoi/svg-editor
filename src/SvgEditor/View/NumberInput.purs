85f9b87ab4e1941cb4c6b36f71c57e1a92a2c878
rizzzse <rizzzse@gmail.com>
2022-08-05 09:26:46 +0900

    feat: NumberInputにonWheel追加

--- src/SvgEditor/View/NumberInput.purs
+++ src/SvgEditor/View/NumberInput.purs
@@ -4,12 +4,15 @@ module SvgEditor.View.NumberInput
 
 import Prelude
 import Data.Number (fromString)
-import Data.Maybe (isNothing)
+import Data.Maybe (maybe, isNothing)
 import Effect.Aff (Aff)
 import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
 import Halogen.HTML.Properties as HP
 import Halogen.HTML.Properties.ARIA as ARIA
+import Web.Event.Event (preventDefault)
+import Web.UIEvent.WheelEvent (toEvent, toMouseEvent, deltaY)
+import Web.UIEvent.MouseEvent (ctrlKey)
 import SvgEditor.View.InputControl (inputControl, inputControlActions, Slot)
 
 numberInput :: forall a. String -> Number -> (Number -> a) -> HH.ComponentHTML a (Slot Number) Aff
@@ -29,6 +32,15 @@ numberInput key value onChange =
       , HE.onValueInput inputControlActions.change
       , HE.onFocus \_ -> inputControlActions.focus
       , HE.onBlur \_ -> inputControlActions.blur
+      , HE.onWheel \e ->
+          let
+            dx = deltaY e / if e # toMouseEvent # ctrlKey then 1000.0 else 100.0
+          in
+            inputControlActions.effect (preventDefault $ e # toEvent)
+              <> ( inputControlActions.change
+                    $ fromString string
+                    # maybe string \x -> show $ x - dx
+                )
       , HP.class_ $ HH.ClassName "input"
       , ARIA.invalid $ show $ fromString string # isNothing
       ]

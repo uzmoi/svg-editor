217c5084b048629243cc17128c9735dba5c90a7a
rizzzse <rizzzse@gmail.com>
2022-08-14 12:34:30 +0900

    app

---
+++ src/app/App.svelte
@@ -0,0 +1,59 @@
+<div class="root">
+  <div class="header">
+    <!-- header -->
+  </div>
+  <div class="main">
+    <div class="center-panel">
+      <!-- canvas -->
+    </div>
+    <div class="right-panel">
+      <!-- layer info -->
+    </div>
+  </div>
+</div>
+
+<style lang="scss">
+  .root {
+    display: flex;
+    flex-direction: column;
+    width: 100vw;
+    height: 100vh;
+    overflow: hidden;
+
+    @media (prefers-color-scheme: light) {
+      --bg-primary: #eee;
+      --bg-secondary: #ddd;
+      --accent: #88d;
+      --text: #333;
+      --error: #d44;
+    }
+    @media (prefers-color-scheme: dark) {
+      --bg-primary: #333;
+      --bg-secondary: #222;
+      --accent: #448;
+      --text: #eee;
+      --error: #c44;
+    }
+
+    font-family: "Noto Sans JP", sans-serif;
+    color: var(--text);
+    background-color: var(--bg-secondary);
+  }
+  .header {
+    height: 8em;
+    background-color: var(--bg-primary);
+  }
+  .main {
+    display: flex;
+    height: 100%;
+  }
+  .center-panel {
+    flex-grow: 1;
+  }
+  .right-panel {
+    display: flex;
+    flex-direction: column;
+    width: 16em;
+    background-color: var(--bg-primary);
+  }
+</style>

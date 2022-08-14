684d049da0fd34f892af6bd9e49256c5d7ef3a8b
rizzzse <rizzzse@gmail.com>
2022-08-14 15:57:59 +0900

    cursorPos

--- src/app/App.svelte
+++ src/app/App.svelte
@@ -1,15 +1,29 @@
 <script lang="ts">
+  import { Vec2 } from "../lib/vec";
   import Header from "./Header.svelte";
   import Canvas from "./canvas/Canvas.svelte";
+
+  let canvasContainer: HTMLDivElement;
+
+  let cursorPos = Vec2.square(0);
+
+  const handleCenterPanelMouseMove: svelte.JSX.MouseEventHandler<HTMLDivElement> = e => {
+    const canvasRect = canvasContainer.getBoundingClientRect();
+    const offset = new Vec2(e.clientX - canvasRect.left, e.clientY - canvasRect.top);
+    const width = 100;
+    const height = 100;
+    const canvasRate = new Vec2(width / canvasRect.width, height / canvasRect.height);
+    cursorPos = offset.mul(canvasRate);
+  };
 </script>
 
 <div class="root">
   <div class="header">
-    <Header />
+    <Header {cursorPos} />
   </div>
   <div class="main">
-    <div class="center-panel">
-      <Canvas />
+    <div class="center-panel" on:mousemove={handleCenterPanelMouseMove}>
+      <Canvas bind:container={canvasContainer} />
     </div>
     <div class="right-panel">
       <!-- layer info -->
--- src/app/Header.svelte
+++ src/app/Header.svelte
@@ -1,5 +1,12 @@
+<script lang="ts">
+  import type { Vec2 } from "../lib/vec";
+
+  export let cursorPos: Vec2<number>;
+</script>
+
 <header>
   <h1>Svg Editor</h1>
+  <p>{cursorPos.x.toFixed(1)}, {cursorPos.y.toFixed(1)}</p>
 </header>
 
 <style lang="scss">

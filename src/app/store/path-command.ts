4a4ac080c912cff6c71e48e119b99fc320167fc3
rizzzse <rizzzse@gmail.com>
2022-08-16 13:15:21 +0900

    PathCommand

--- src/app/store/path-command.ts
+++ src/app/store/path-command.ts
@@ -1,10 +1,27 @@
 import type { Vec2 } from "~/lib/vec";
 
 export type PathCommand =
-  | { type: "M"; to: Vec2<number> }
-  | { type: "L"; to: Vec2<number> }
-  | { type: "T"; to: Vec2<number> }
-  | { type: "Q"; v1: Vec2<number>; to: Vec2<number> }
-  | { type: "S"; v2: Vec2<number>; to: Vec2<number> }
-  | { type: "C"; v1: Vec2<number>; v2: Vec2<number>; to: Vec2<number> }
-  | { type: "Z" };
+  | {
+      type: "Move" | "Line";
+      to: Vec2<number>;
+    }
+  | {
+      type: "Bezier2";
+      v1: Vec2<number> | null;
+      to: Vec2<number>;
+    }
+  | {
+      type: "Bezier3";
+      v1: Vec2<number> | null;
+      v2: Vec2<number>;
+      to: Vec2<number>;
+    }
+  | {
+      type: "Arc";
+      radius: Vec2<number>;
+      rotation: number;
+      largeArc: 0 | 1;
+      sweep: 0 | 1;
+      to: Vec2<number>;
+    }
+  | { type: "Close" };
